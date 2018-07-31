5f7267febc4aec2cdb8da81ec93d8f38ed3b321d
lbgod2222 <1097843969@qq.com>
2018-07-31 16:40:19 +0800

    chore(action page): init actions

--- src/store/actions.js
+++ src/store/actions.js
@@ -3,10 +3,35 @@ import api from '../utils/api'
 // import { getCache } from '../utils/util'
 
 export default {
+  // 获取账户详情
   getAccount: ({
     commit,
     state
   }, address) => {
-    return api.account(address)
+    return api.accounts(address)
+  },
+  // HASH 值查询
+  getTransactionInfo: ({commit, state}, tid) => {
+    return api.transaction(tid)
+  },
+  // 获取区块列表
+  getBlocks: ({commit, state}, params) => {
+    return api.blocks(params)
+  },
+  // 获取交易列表
+  getTransactions: ({commit, state}, params) => {
+    return api.transactions(params)
+  },
+  // 获取受托人列表
+  getDelegates: ({commit, state}, params) => {
+    return api.delegates(params)
+  },
+  // 获取资产列表
+  getAssets: ({commit, state}, params) => {
+    return api.assets(params)
+  },
+  // 获取资产详情
+  getAssetInfo: ({commit, state}, params) => {
+    return api.uiaAsset(params)
   }
 }
